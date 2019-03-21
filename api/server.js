85f8bae00be998bee4192a6604e22262a5822aa2
mkubibaev <mkubibaev@gmail.com>
2019-03-21 14:21:33 +0600

    api init

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Server started on ${port} port`);
+});
